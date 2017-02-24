85d95502612fc712844cc449f8c63fd46729f316
illusion030 <illusion030@gmail.com>
2017-02-25 01:33:38 +0800

    Delete the dead code.

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -19,15 +19,6 @@ typedef struct __PHONE_BOOK_ENTRY_DETAILS {
 
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
-    /*  char firstName[16];
-      char email[16];
-      char phone[10];
-      char cell[10];
-      char addr1[16];
-      char addr2[16];
-      char city[16];
-      char state[2];
-      char zip[5];*/
     details *detail;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
