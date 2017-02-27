3f9a2717740ebc6aacfe9455d2d2d57bbecce8bd
illusion030 <illusion030@gmail.com>
2017-02-27 17:16:30 +0800

    Delete the extra blank line

--- phonebook_combine.h
+++ phonebook_combine.h
@@ -28,5 +28,5 @@ typedef struct __PHONE_BOOK_ENTRY {
 entry *findName(char lastName[], entry *pHead);
 entry *append(char lastName[], entry *e);
 entry *LinkedlistToBSTRecur(int count, entry **root);
-#endif
 
+#endif
